9baea8276e2a07dbf2e05b041a0e15b4be5787f8
Andi Nugroho <andi.nugroho@autodesk.com>
2017-04-22 15:01:49 +0800

    Added ml_modeling.py

---
+++ ml_modeling.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 20:59:42 2017
+
+@author: ongby
+"""
+
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import sklearn
+
+
+data = pd.read_csv("C:\Users\Andy\Google Drive\School\EB5001 Big Data Engineering for Analytics\Project 2\Datasets\final_data3.csv")
+
+data = data.fillna(0)
+A = data.columns
+A =  A.drop("Risk of heart diseases")
+A =  A.drop("SEQN")
+X = data[A]
+y = data["Risk of heart diseases"]
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.33, random_state=42)
+
+mod = sm.OLS(y_train,X_train)
+
+results = mod.fit()
+print(results.summary())
+
+sm.add_constant(X_test)
+
+
